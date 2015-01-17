404da232212a6c3e9a0fea0eee59a7f6a40c3448
adityahiran <p.adityahiran@gmail.com>
2015-01-16 23:08:41 -0800

    Refactored to used the Facade design pattern to wrap the Rest API

---
+++ src/main/java/com/mashape/interview/ToDoHerokuApp/todofacade/ToDoFacade.java
@@ -0,0 +1,100 @@
+package com.mashape.interview.ToDoHerokuApp.todofacade;
+
+import java.util.List;
+import java.util.Set;
+
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.mashape.interview.ToDoHerokuApp.domains.Item;
+
+@Path("todo-list-items")
+public class ToDoFacade {
+
+	private static ToDoFacade instance = null;
+	private ToDoWrapper toDoWrapper = ToDoWrapper.getInstance();
+
+	public static ToDoFacade getInstance() {
+		if (instance == null) {
+			instance = new ToDoFacade();
+		}
+		return instance;
+	}
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Set<Item> getAllItems() {
+		return toDoWrapper.getAllItems();
+	}
+
+	// Get the item with the specified title in the to-do list - SUCCESS
+	@GET
+	@Path("{title}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Item getItemByTitle(@PathParam("title") String title) {
+		return toDoWrapper.getItemByTitle(title);
+	}
+
+	// Get all the completed todo list items - SUCCESS
+	@GET
+	@Path("done")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Set<Item> getCompletedItems() {
+		return toDoWrapper.getCompletedItems();
+	}
+
+	// Get all the todo list items that are yet to be completed - SUCCESS
+	@GET
+	@Path("undone")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Set<Item> getItemsYetToComplete() {
+		return toDoWrapper.getItemsYetToComplete();
+	}
+
+	@GET
+	@Path("search/{searchTerm}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Item> searchItem(@PathParam("searchTerm") String searchTerm) {
+		return toDoWrapper.searchItem(searchTerm);
+	}
+
+	@POST
+	@Path("{title}/{body}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Item saveItemByTitle(@NotNull @PathParam("title") String title,
+			@PathParam("body") String body) {
+		return toDoWrapper.saveItemByTitle(title, body);
+	}
+
+	@DELETE
+	@Path("/{title}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Item deleteItemByTitle(@NotNull @PathParam("title") String title) {
+		return toDoWrapper.deleteItemByTitle(title);
+	}
+
+	// HTTP REQUEST METHOD: PUT
+	@PUT
+	@Path("{old-title}/{new-title}/{new-body}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public void updateItem(@NotNull @PathParam("old-title") String oldTitle,
+			@NotNull @PathParam("new-title") String newTitle,
+			@PathParam("new-body") String newBody) {
+		toDoWrapper.updateItem(oldTitle, newTitle, newBody);
+	}
+
+	// Mark a todo list item as done
+	@PUT
+	@Path("{title}/mark-as-done")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Item markItemAsDone(@NotNull @PathParam("title") String title) {
+		return toDoWrapper.markItemAsDone(title);
+	}
+}
---
+++ src/main/java/com/mashape/interview/ToDoHerokuApp/todofacade/ToDoWrapper.java
@@ -0,0 +1,61 @@
+package com.mashape.interview.ToDoHerokuApp.todofacade;
+
+import java.util.List;
+import java.util.Set;
+
+import com.mashape.interview.ToDoHerokuApp.domains.Item;
+import com.mashape.interview.ToDoHerokuApp.services.SearchService;
+import com.mashape.interview.ToDoHerokuApp.services.ToDoService;
+
+class ToDoWrapper {
+
+	private ToDoService toDoService = ToDoService.getInstance();
+	private SearchService searchService = SearchService.getInstance();
+	
+	private static ToDoWrapper instance = null;
+	
+	public static ToDoWrapper getInstance() {
+		if(instance == null) {
+			instance = new ToDoWrapper();
+		}
+		return instance;
+	}
+	
+	public Set<Item> getAllItems() {
+        return toDoService.getAllItems();
+    }
+	
+	public Item getItemByTitle(String title) {
+		return toDoService.getItemByTitle(title);
+    }
+	
+	public Set<Item> getCompletedItems() {
+		boolean status = true;
+    	return toDoService.getItemsByStatus(status);
+    }
+	
+	public Set<Item> getItemsYetToComplete() {
+		boolean status = false;
+    	return toDoService.getItemsByStatus(status);
+    }
+    
+    public List<Item> searchItem(String searchTerm) {
+    	return searchService.searchItems(searchTerm);
+    }
+    
+    public Item saveItemByTitle(String title,  String body ) {
+    	return toDoService.saveItem(title, body, false);	
+    }
+    
+    public Item deleteItemByTitle(String title) {
+    	return toDoService.deleteItemByTitle(title);
+    }
+    
+    public void updateItem(String oldTitle, String newTitle,  String newBody) {
+    	toDoService.updateItem(oldTitle, newTitle, newBody);
+    }
+    
+    public Item markItemAsDone(String title) {
+    	return toDoService.markItemAsDone(title);
+    }
+}
