9fc407f15afe2cda0999b75bce3b878069cab8a8
adityahiran <p.adityahiran@gmail.com>
2015-01-19 01:25:39 -0800

    debugging twilio notification

--- src/main/java/com/mashape/interview/ToDoHerokuApp/strategies/TwilioStrategy.java
+++ src/main/java/com/mashape/interview/ToDoHerokuApp/strategies/TwilioStrategy.java
@@ -61,23 +61,24 @@ public class TwilioStrategy implements INotifyStrategy {
 		
 		// Step1
 		Client client = ClientBuilder.newClient();
-		String url = "https://api.twilio.com/2010-04-01/Accounts/AC712fe2e4e8f5620f46d435a6dae8ab3e/Messages.json";
+		String url = "https://api.twilio.com/2010-04-01/Accounts/ACad0b05acb0ff8d54e96a5684cdeed25d/Messages.json";
 		
-		HttpAuthenticationFeature feature = HttpAuthenticationFeature.basicBuilder().credentials("AC712fe2e4e8f5620f46d435a6dae8ab3e", "e5f01f52d8d37dbb142366a095819875").build();
+		//HttpAuthenticationFeature feature = HttpAuthenticationFeature.basicBuilder().credentials("AC712fe2e4e8f5620f46d435a6dae8ab3e", "e5f01f52d8d37dbb142366a095819875").build();
+		HttpAuthenticationFeature feature = HttpAuthenticationFeature.basicBuilder().credentials("ACad0b05acb0ff8d54e96a5684cdeed25d", "0263a083588152039b192a5ff9143ff1").build();
 		client.register(feature);
 		
 		// Step2
 		WebTarget target = client.target(url);
 				
 		MultivaluedHashMap<String, String> postForm = new MultivaluedHashMap<String, String>();
-		postForm.add("From", "+15005550006");
+		postForm.add("From", "+19164321120");
 		postForm.add("To", "+19168137782");
 		postForm.add("Body", "Hello body");
 		
 		// Step3
 		Response response = target.request().post(Entity.form(postForm));
 		
-		if((response.getStatus() == 200) || (response.getStatus() == 201)) return response.toString();
+		if((response.getStatus() == 200) || (response.getStatus() == 201)) return response.toString()+" successfully sent the notification.";
 		return "Error sending the notification";
 	}
 }
