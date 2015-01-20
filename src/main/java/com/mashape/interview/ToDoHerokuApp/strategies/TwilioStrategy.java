494383a988488f984c1979d6a8cc978d836c4e55
adityahiran <p.adityahiran@gmail.com>
2015-01-20 05:12:20 -0800

    debugging

--- src/main/java/com/mashape/interview/ToDoHerokuApp/strategies/TwilioStrategy.java
+++ src/main/java/com/mashape/interview/ToDoHerokuApp/strategies/TwilioStrategy.java
@@ -5,6 +5,7 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MultivaluedHashMap;
+import javax.ws.rs.core.Response;
 
 import com.mashape.interview.ToDoHerokuApp.domains.Item;
 import com.mashape.interview.ToDoHerokuApp.utilities.ToDoAppConstants;
@@ -27,9 +28,11 @@ public class TwilioStrategy implements INotifyStrategy {
 		
 		// Step1
 		Client client = ClientBuilder.newClient();
-		String url = ToDoAppConstants.getInstance().getTwilioSmsUrl();
+		//String url = ToDoAppConstants.getInstance().getTwilioSmsUrl();
+		String url = "https://api.twilio.com/2010-04-01/Accounts/ACad0b05acb0ff8d54e96a5684cdeed25d/Messages.json";
 		
-		HttpAuthenticationFeature feature = HttpAuthenticationFeature.basicBuilder().credentials(ToDoAppConstants.getInstance().getTwilioAccountSid(), ToDoAppConstants.getInstance().getTwilioAccountPassword()).build();
+		//HttpAuthenticationFeature feature = HttpAuthenticationFeature.basicBuilder().credentials(ToDoAppConstants.getInstance().getTwilioAccountSid(), ToDoAppConstants.getInstance().getTwilioAccountPassword()).build();
+		HttpAuthenticationFeature feature = HttpAuthenticationFeature.basicBuilder().credentials("ACad0b05acb0ff8d54e96a5684cdeed25d", "0263a083588152039b192a5ff9143ff1").build();
 		client.register(feature);
 		
 		// Step2
@@ -41,6 +44,6 @@ public class TwilioStrategy implements INotifyStrategy {
 		postForm.add("Body", lastModiefiedItem.getTitle() + " has been marked as done on the todo list.");
 		
 		// Step3
-		target.request().post(Entity.form(postForm));
+		Response r = target.request().post(Entity.form(postForm));
 	}
 }
