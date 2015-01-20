de34529c6feab837b541406f919aab4701375099
adityahiran <p.adityahiran@gmail.com>
2015-01-20 08:47:31 -0800

    Added comments

--- src/main/java/com/mashape/interview/ToDoHerokuApp/utilities/ToDoAppConstants.java
+++ src/main/java/com/mashape/interview/ToDoHerokuApp/utilities/ToDoAppConstants.java
@@ -1,24 +1,28 @@
 package com.mashape.interview.ToDoHerokuApp.utilities;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
 public class ToDoAppConstants {
 
 	private static ToDoAppConstants instance = null;
-	private static final String DB_NAME = "todo-db";
-	private static final String COLLECTION_NAME = "items";
+	
+	// For searchbox.io
 	private static final String JEST_URL= "https://site:feadc2f4d2e573c710cad584185a8965@bofur-us-east-1.searchly.com";
+	
+	// For twilio
 	private static String TWILIO_ACCOUNT_SID = "ACad0b05acb0ff8d54e96a5684cdeed25d"; // This could be made more secure on a production level code.
 	private static String TWILIO_ACCOUNT_PASSWORD = "0263a083588152039b192a5ff9143ff1";
 	private static final String TWILIO_FROM_NUMBER = "+19164321120";
 	private static final String TWILIO_TO_NUMBER = "+19168137782";
 	private static String TWILIO_SMS_URL ="https://api.twilio.com/2010-04-01/Accounts/"+ToDoAppConstants.getInstance().getTwilioAccountSid()+"/Messages.json";
+	
+	// For the Observer-Observable pattern that keeps the database and the searchbox.io indexes in sync 
 	private static final int CRUD_CREATE = 1;
 	private static final int CRUD_UPDATE = 2;
 	private static final int CRUD_MARK_AS_DONE = 4;
 	private static final int CRUD_DELETE = 3;
+	
+	// For mongodb if used
+	private static final String DB_NAME = "todo-db";
+	private static final String COLLECTION_NAME = "items";
 	
 	public int getCrudCreate() {
 		return CRUD_CREATE;
@@ -62,7 +66,7 @@ public class ToDoAppConstants {
 		}
 		return instance;
 	}
-	
+
 	public String getJesturl() {
 		return JEST_URL;
 	}
