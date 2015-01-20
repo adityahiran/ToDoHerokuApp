b8e468a62c90ab93bd86f53509c0b2a9a9eea122
adityahiran <p.adityahiran@gmail.com>
2015-01-20 03:27:56 -0800

    Added WebApplicaitonExceptions to handle the exceptional cases.

--- src/main/java/com/mashape/interview/ToDoHerokuApp/todofacade/ToDoFacade.java
+++ src/main/java/com/mashape/interview/ToDoHerokuApp/todofacade/ToDoFacade.java
@@ -5,6 +5,7 @@ import java.net.URISyntaxException;
 import java.util.Set;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.executable.ValidateOnExecution;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.FormParam;
@@ -14,6 +15,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -79,11 +81,18 @@ public class ToDoFacade {
 	}
 
 	// Save a new todo-list-item - WORKING
+	// Add the location of the created resource to the response header
 	@POST
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response saveItemByTitle(@NotNull @FormParam("title") String title,
+	public Response saveItemByTitle(@NotNull@FormParam("title") String title,
 			@FormParam("body") String body) {
+		
+		// Validation at the application level
+		if(title==null || title.isEmpty()) {
+			throw new WebApplicationException("Cannot create a todo-list-item without a valid title as a form parameter.", 400);
+		}
+		
 		Item item = toDoWrapper.saveItemByTitle(title, body);
 		URI uri = null;
 		try {
@@ -91,6 +100,8 @@ public class ToDoFacade {
 		} catch (URISyntaxException e) {
 			e.printStackTrace();
 		}
+		
+		// Response has a status code of 201 with a location header and a body
 		return Response.created(uri).entity(item).build();
 	}
 
@@ -98,7 +109,13 @@ public class ToDoFacade {
 	@DELETE
 	@Path("/{title}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response deleteItemByTitle(@NotNull @PathParam("title") String title) {
+	public Response deleteItemByTitle(@NotNull@PathParam("title") String title) {
+		
+		// Application level Validation
+		if(title==null || title.isEmpty()) {
+			throw new WebApplicationException("Cannot delete a todo-list-item with a null title.", 400);
+		}
+		
 		Item item = toDoWrapper.deleteItemByTitle(title);
 		return Response.ok(item).build();
 	}
@@ -111,6 +128,16 @@ public class ToDoFacade {
 	public Response updateItem(@NotNull @PathParam("old-title") String oldTitle,
 			@NotNull@FormParam("new-title") String newTitle,
 			@FormParam("new-body") String newBody) {
+		
+		// Application level validation
+		if(oldTitle==null || oldTitle.isEmpty()) {
+			throw new WebApplicationException("While updating a todo-list-item, you should provide the title of the item you want to update. The title should be not null and not empty.", 400);
+		}
+		
+		if(newTitle==null || newTitle.isEmpty()) {
+			throw new WebApplicationException("While updating a todo-list-item, you should provide the new title of the item you want to update. The title should be not null and not empty.", 400);
+		}
+		
 		Item item = toDoWrapper.updateItem(oldTitle, newTitle, newBody);
 		return Response.ok(item).build();
 	}
@@ -120,6 +147,12 @@ public class ToDoFacade {
 	@Path("{title}/mark-as-done")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response markItemAsDone(@NotNull @PathParam("title") String title) {
+		
+		// Application level Validation
+		if(title==null || title.isEmpty()) {
+			throw new WebApplicationException("Cannot mark a todo-list-item with a null title as done.", 400);
+		}
+		
 		Item item = toDoWrapper.markItemAsDone(title);
 		return Response.ok(item).build();
 	}
